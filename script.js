120ff4d1db44c434a9352f93e5b74104160ee13b
Alex Liebhaber <alexliebhaber1992@gmail.com>
2023-09-01 20:16:58 -0500

    added ui and displaying winner/loser text in dom

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+// DOM
+const rockBtn = document.querySelector(".rock-btn");
+const paperBtn = document.querySelector(".paper-btn");
+const scissorsBtn = document.querySelector(".scissors-btn");
+const results = document.querySelector(".results");
+
+rockBtn.addEventListener("click", () => {
+  const playerChoice = "rock";
+  const computerChoice = getComputerChoice();
+  round(playerChoice, computerChoice);
+  console.log(playerChoice, computerChoice);
+});
+paperBtn.addEventListener("click", () => {
+  const playerChoice = "paper";
+  const computerChoice = getComputerChoice();
+  round(playerChoice, computerChoice);
+  console.log(playerChoice, computerChoice);
+});
+scissorsBtn.addEventListener("click", () => {
+  const playerChoice = "scissors";
+  const computerChoice = getComputerChoice();
+  round(playerChoice, computerChoice);
+  console.log(playerChoice, computerChoice);
+});
+
+// Game Logic
+
 function randomNumber() {
   return Math.random();
 }
@@ -10,46 +37,58 @@ function getComputerChoice() {
   else if (num <= 0.66) computerChoice = "paper";
   else computerChoice = "scissors";
 
-  console.log(computerChoice);
+  //   console.log(computerChoice);
 
   return computerChoice;
 }
 
-// let computerChoice = getComputerChoice();
+let computerChoice = getComputerChoice();
+
+// used for console game
+
 // let playerChoice = prompt("Rock, Paper, or Scissors?");
 // playerChoice = playerChoice.toLowerCase();
 
-let playerWins = 0;
-let computerWins = 0;
-
 function round(playerChoice, computerChoice) {
+  let playerWins = 0;
+  let computerWins = 0;
+
   if (playerChoice === "rock" && computerChoice === "scissors") {
-    return playerWins++;
+    playerWins++;
   } else if (playerChoice === "rock" && computerChoice === "paper") {
-    return computerWins++;
+    computerWins++;
   } else if (playerChoice === "paper" && computerChoice === "rock") {
-    return playerWins++;
+    playerWins++;
   } else if (playerChoice === "paper" && computerChoice === "scissors") {
-    return computerWins++;
+    computerWins++;
   } else if (playerChoice === "scissors" && computerChoice === "paper") {
-    return playerWins++;
+    playerWins++;
   } else if (playerChoice === "scissors" && computerChoice === "rock") {
-    return computerWins++;
-  } else if (playerChoice === computerChoice) {
-    return null;
-  }
-}
-
-function game() {
-  for (let i = 0; i < 5; i++) {
-    const computerChoice = getComputerChoice();
-    const playerChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
-
-    round(computerChoice, playerChoice);
-    console.log(playerWins, computerWins);
+    computerWins++;
   }
 
-  console.log(playerWins > computerWins ? "You win" : "Computer win");
+  console.log(playerWins, computerWins);
+
+  results.textContent =
+    playerWins > computerWins
+      ? `You win ${playerChoice} beats ${computerChoice}`
+      : playerWins === computerWins
+      ? `You both chose ${playerChoice}`
+      : `You lose ${computerChoice} beats ${playerChoice}`;
 }
 
-game();
+// best of 5 game - in console - to play, move playerWins and computerWins to global vars and uncomment below code
+
+// function game() {
+//   for (let i = 0; i < 5; i++) {
+//     const computerChoice = getComputerChoice();
+//     const playerChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
+
+//     round(computerChoice, playerChoice);
+//     console.log(playerWins, computerWins);
+//   }
+
+//   console.log(playerWins > computerWins ? "You win" : "Computer win");
+// }
+
+// game();
